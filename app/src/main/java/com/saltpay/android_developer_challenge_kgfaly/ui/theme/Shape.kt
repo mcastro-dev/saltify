d257c0d1f438798f8015ee8be2ff5a2d944277d0
mcr66 <matheus.castro@sky.uk>
2023-01-01 15:56:04 +0000

    Start project

---
+++ app/src/main/java/com/saltpay/android_developer_challenge_kgfaly/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.saltpay.android_developer_challenge_kgfaly.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
